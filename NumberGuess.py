2e6d6dc6f938d9e0ba44664c67cc5dd121c83ffd
jugat <jugat@EAGLES.fios-router.home>
2018-11-14 18:42:30 -0500

    Finished it

--- NumberGuess.py
+++ NumberGuess.py
@@ -11,8 +11,8 @@ guess = int(input("Please enter a number that you guess between 1 to 20: "))
 
 if (guess>=1 and guess<=20):
         if (guess==num):
-            print('You have guessed the correct number. The number was ', num,'.')
+            print('You have guessed the correct number. The number was ', num)
         else:
-            print('You have guessed the wrong number. The number was ', num,'.')
+            print('You have guessed the wrong number. The number was ', num)
 else:
     print('You have entered an invalid number.')

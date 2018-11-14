b3be7d55f6f466e1d8154169bc7b2ae501f3644e
singhj22 <42975589+singhj22@users.noreply.github.com>
2018-11-14 12:44:28 -0500

    Add files via upload

---
+++ NumberGuess.py
@@ -0,0 +1,15 @@
+import random
+'''
+
+@author: Jugat Singh
+Number Guess
+
+'''
+
+num= random.randrange(1,21)
+
+guess = int(input("Please enter a number that you guess between 1 to 20: "))
+
+if (guess>=1 and guess<=20):
+    
+
